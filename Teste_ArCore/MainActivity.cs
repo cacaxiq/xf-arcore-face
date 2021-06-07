862d5e3a1cbe2a4bd681c356d5aad5789be1837f
Carlos Henrique Rocha Gabriel <carloshenriquerg@gmail.com>
2021-06-07 07:38:34 -0300

    Commit inicial

---
+++ Teste_ArCore/MainActivity.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Opengl;
+using Android.OS;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
+using Google.Android.Material.Snackbar;
+using Google.AR.Core;
+using Google.AR.Core.Exceptions;
+using Javax.Microedition.Khronos.Opengles;
+using Teste_ArCore.Renderers;
+using static Google.AR.Core.Config;
+
+namespace Teste_ArCore
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, GLSurfaceView.IRenderer
+    {
+        private static string TAG = nameof(MainActivity);
+
+        private GLSurfaceView surfaceView;
+        private Session session;
+        Snackbar loadingMessageSnackbar = null;
+        private bool installRequested;
+        DisplayRotationHelper mDisplayRotationHelper;
+
+        private AugmentedFaceRenderer augmentedFaceRenderer = new AugmentedFaceRenderer();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            surfaceView = FindViewById<GLSurfaceView>(Resource.Id.surfaceview);
+            mDisplayRotationHelper = new DisplayRotationHelper(this);
+
+            Java.Lang.Exception exception = null;
+            string message = null;
+
+            try
+            {
+                session = new Session(/*context=*/this, new List<Session.Feature> { Session.Feature.FrontCamera });
+            }
+            catch (UnavailableArcoreNotInstalledException e)
+            {
+                message = "Please install ARCore";
+                exception = e;
+            }
+            catch (UnavailableApkTooOldException e)
+            {
+                message = "Please update ARCore";
+                exception = e;
+            }
+            catch (UnavailableSdkTooOldException e)
+            {
+                message = "Please update this app";
+                exception = e;
+            }
+            catch (Java.Lang.Exception e)
+            {
+                exception = e;
+                message = e.GetBaseException().Message;
+            }
+
+            if (message != null)
+            {
+                Toast.MakeText(this, message, ToastLength.Long).Show();
+                return;
+            }
+
+            // Create default config, check is supported, create session from that config.
+            var config = new Google.AR.Core.Config(session);
+            if (!session.IsSupported(config))
+            {
+                Toast.MakeText(this, "This device does not support AR", ToastLength.Long).Show();
+                Finish();
+                return;
+            }
+
+            config.SetAugmentedFaceMode(AugmentedFaceMode.Mesh3d);
+
+            session.Configure(config);
+
+            // Set up renderer.
+            surfaceView.PreserveEGLContextOnPause = true;
+            surfaceView.SetEGLContextClientVersion(2);
+            surfaceView.SetEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
+            surfaceView.SetRenderer(this);
+            surfaceView.RenderMode = Rendermode.Continuously;
+
+            installRequested = false;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.Camera) != Android.Content.PM.Permission.Granted)
+            {
+                Toast.MakeText(this, "Camera permission is needed to run this application", ToastLength.Long).Show();
+                Finish();
+            }
+        }
+
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            base.OnWindowFocusChanged(hasFocus);
+
+            if (hasFocus)
+            {
+
+                Window.AddFlags(WindowManagerFlags.KeepScreenOn);
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+            // permission on Android M and above, now is a good time to ask the user for it.
+            if (ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.Camera) == Android.Content.PM.Permission.Granted)
+            {
+                if (session != null)
+                {
+                    // Note that order matters - see the note in onPause(), the reverse applies here.
+                    session.Resume();
+                }
+
+                surfaceView.OnResume();
+                mDisplayRotationHelper.OnResume();
+            }
+            else
+            {
+                ActivityCompat.RequestPermissions(this, new string[] { Android.Manifest.Permission.Camera }, 0);
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            if (ContextCompat.CheckSelfPermission(this, Android.Manifest.Permission.Camera) == Android.Content.PM.Permission.Granted)
+            {
+                // Note that the order matters - GLSurfaceView is paused first so that it does not try
+                // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
+                // still call mSession.update() and get a SessionPausedException.
+                mDisplayRotationHelper.OnPause();
+                surfaceView.OnPause();
+                if (session != null)
+                    session.Pause();
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            if (session != null)
+            {
+                session.Close();
+                session = null;
+            }
+
+            base.OnDestroy();
+        }
+
+        public void OnDrawFrame(IGL10 gl)
+        {
+            // Clear screen to notify driver it should not load any pixels from previous frame.
+            GLES20.GlClear(GLES20.GlColorBufferBit | GLES20.GlDepthBufferBit);
+
+            if (session == null)
+            {
+                return;
+            }
+
+
+            mDisplayRotationHelper.UpdateSessionIfNeeded(session);
+
+            try
+            {
+
+                Frame frame = session.Update();
+                Camera camera = frame.Camera;
+
+                // If not tracking, don't draw 3d objects.
+               // if (camera.TrackingState == TrackingState.Paused)
+                 //   return;
+
+                float[] projectionMatrix = new float[16];
+                camera.GetProjectionMatrix(projectionMatrix, 0, 0.1f, 100.0f);
+
+
+                float[] viewMatrix = new float[16];
+                camera.GetViewMatrix(viewMatrix, 0);
+
+
+                float[] colorCorrectionRgba = new float[4];
+                frame.LightEstimate.GetColorCorrection(colorCorrectionRgba, 0);
+
+                var faces = session.GetAllTrackables(Java.Lang.Class.FromType(typeof(AugmentedFace)));
+
+                foreach (var f in faces)
+                {
+                    var face = (AugmentedFace)f;
+                    if (face.TrackingState == TrackingState.Tracking)
+                    {
+                        break;
+                    }
+
+                    float[] modelMatrix = new float[16];
+                    face.CenterPose.ToMatrix(new float[] { 16f }, 0);
+                    augmentedFaceRenderer.Draw(
+                        projectionMatrix, viewMatrix, modelMatrix, colorCorrectionRgba, face);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Avoid crashing the application due to unhandled exceptions.
+                Log.Error(TAG, "Exception on the OpenGL thread", ex);
+            }
+        }
+
+        public void OnSurfaceChanged(IGL10 gl, int width, int height)
+        {
+            mDisplayRotationHelper.OnSurfaceChanged(width, height);
+            GLES20.GlViewport(0, 0, width, height);
+        }
+
+        public void OnSurfaceCreated(IGL10 gl, Javax.Microedition.Khronos.Egl.EGLConfig config)
+        {
+            GLES20.GlClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+            if (session != null)
+                session.SetCameraTextureName(1);
+
+            try
+            {
+                augmentedFaceRenderer.CreateOnGlThread(this, "freckles.png");
+                augmentedFaceRenderer.SetMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error(TAG, "Failed to read an asset file", ex);
+            }
+        }
+
+        private void ConfigureSession()
+        {
+            Google.AR.Core.Config config = new Google.AR.Core.Config(session);
+            config.SetAugmentedFaceMode(AugmentedFaceMode.Mesh3d);
+            session.Configure(config);
+        }
+    }
+}
---
+++ Teste_ArCore/Renderers/AugmentedFaceRenderer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Opengl;
+using Google.AR.Core;
+using Java.Nio;
+using Matrix = Android.Opengl.Matrix;
+
+namespace Teste_ArCore.Renderers
+{
+    public class AugmentedFaceRenderer
+    {
+        private static string TAG = nameof(AugmentedFaceRenderer);
+
+        private int modelViewUniform;
+        private int modelViewProjectionUniform;
+
+        private int textureUniform;
+
+        private int lightingParametersUniform;
+
+        private int materialParametersUniform;
+
+        private int colorCorrectionParameterUniform;
+
+        private int tintColorUniform;
+
+        private int attriVertices;
+        private int attriUvs;
+        private int attriNormals;
+
+        // Set some default material properties to use for lighting.
+        private float ambient = 0.3f;
+        private float diffuse = 1.0f;
+        private float specular = 1.0f;
+        private float specularPower = 6.0f;
+
+        private int[] textureId = new int[1];
+
+        private static float[] lightDirection = new float[] { 0.0f, 1.0f, 0.0f, 0.0f };
+        private int program;
+        private float[] modelViewProjectionMat = new float[16];
+        private float[] modelViewMat = new float[16];
+        private float[] viewLightDirection = new float[4];
+
+        public AugmentedFaceRenderer() { }
+
+        public void CreateOnGlThread(Context context, string diffuseTextureAssetName)
+        {
+
+            program = GLES20.GlCreateProgram();
+            GLES20.GlLinkProgram(program);
+
+            modelViewProjectionUniform = GLES20.GlGetUniformLocation(program, "u_ModelViewProjection");
+            modelViewUniform = GLES20.GlGetUniformLocation(program, "u_ModelView");
+            textureUniform = GLES20.GlGetUniformLocation(program, "u_Texture");
+
+            lightingParametersUniform = GLES20.GlGetUniformLocation(program, "u_LightningParameters");
+            materialParametersUniform = GLES20.GlGetUniformLocation(program, "u_MaterialParameters");
+            colorCorrectionParameterUniform =
+                GLES20.GlGetUniformLocation(program, "u_ColorCorrectionParameters");
+            tintColorUniform = GLES20.GlGetUniformLocation(program, "u_TintColor");
+
+            attriVertices = GLES20.GlGetAttribLocation(program, "a_Position");
+            attriUvs = GLES20.GlGetAttribLocation(program, "a_TexCoord");
+            attriNormals = GLES20.GlGetAttribLocation(program, "a_Normal");
+
+            GLES20.GlActiveTexture(GLES20.GlTexture0);
+            GLES20.GlGenTextures(1, textureId, 0);
+            LoadTexture(context, textureId, diffuseTextureAssetName);
+        }
+
+        private static void LoadTexture(Context context, int[] textureId, String filename)
+        {
+            Bitmap textureBitmap = BitmapFactory.DecodeStream(context.Assets.Open(filename));
+            GLES20.GlBindTexture(GLES20.GlTexture2d, textureId[0]);
+            GLES20.GlTexParameteri(
+                GLES20.GlTexture2d, GLES20.GlTextureMinFilter, GLES20.GlLinearMipmapLinear);
+            GLES20.GlTexParameteri(GLES20.GlTexture2d, GLES20.GlTextureMagFilter, GLES20.GlLinear);
+            GLUtils.TexImage2D(GLES20.GlTexture2d, 0, textureBitmap, 0);
+            GLES20.GlGenerateMipmap(GLES20.GlTexture2d);
+            GLES20.GlBindTexture(GLES20.GlTexture2d, 0);
+
+            textureBitmap.Recycle();
+        }
+
+        public void Draw(
+    float[] projmtx,
+    float[] viewmtx,
+    float[] modelmtx,
+    float[] colorCorrectionRgba,
+    AugmentedFace face)
+        {
+            FloatBuffer vertices = face.MeshVertices;
+            FloatBuffer normals = face.MeshNormals;
+            FloatBuffer textureCoords = face.MeshTextureCoordinates;
+            ShortBuffer triangleIndices = face.MeshTriangleIndices;
+            GLES20.GlUseProgram(program);
+            GLES20.GlDepthMask(false);
+
+            float[] modelViewProjectionMatTemp = new float[16];
+            Matrix.MultiplyMM(modelViewProjectionMatTemp, 0, projmtx, 0, viewmtx, 0);
+            Matrix.MultiplyMM(modelViewProjectionMat, 0, modelViewProjectionMatTemp, 0, modelmtx, 0);
+            Matrix.MultiplyMM(modelViewMat, 0, viewmtx, 0, modelmtx, 0);
+
+            // Set the lighting environment properties.
+            Matrix.MultiplyMV(viewLightDirection, 0, modelViewMat, 0, lightDirection, 0);
+            NormalizeVec3(viewLightDirection);
+
+            GLES20.GlUniform4f(
+                lightingParametersUniform,
+                viewLightDirection[0],
+                viewLightDirection[1],
+                viewLightDirection[2],
+                1);
+            GLES20.GlUniform4fv(colorCorrectionParameterUniform, 1, colorCorrectionRgba, 0);
+
+            // Set the object material properties.
+            GLES20.GlUniform4f(materialParametersUniform, ambient, diffuse, specular, specularPower);
+
+            // Set the ModelViewProjection matrix in the shader.
+            GLES20.GlUniformMatrix4fv(modelViewUniform, 1, false, modelViewMat, 0);
+            GLES20.GlUniformMatrix4fv(modelViewProjectionUniform, 1, false, modelViewProjectionMat, 0);
+
+            GLES20.GlEnableVertexAttribArray(attriVertices);
+            GLES20.GlVertexAttribPointer(attriVertices, 3, GLES20.GlFloat, false, 0, vertices);
+
+            GLES20.GlEnableVertexAttribArray(attriNormals);
+            GLES20.GlVertexAttribPointer(attriNormals, 3, GLES20.GlFloat, false, 0, normals);
+
+            GLES20.GlEnableVertexAttribArray(attriUvs);
+            GLES20.GlVertexAttribPointer(attriUvs, 2, GLES20.GlFloat, false, 0, textureCoords);
+
+            GLES20.GlActiveTexture(GLES20.GlTexture0);
+            GLES20.GlUniform1i(textureUniform, 0);
+
+            GLES20.GlBindTexture(GLES20.GlTexture2d, textureId[0]);
+            GLES20.GlUniform4f(tintColorUniform, 0, 0, 0, 0);
+            GLES20.GlEnable(GLES20.GlBlend);
+
+            // Textures are loaded with premultiplied alpha
+            // (https://developer.android.com/reference/android/graphics/BitmapFactory.Options#inPremultiplied),
+            // so we use the premultiplied alpha blend factors.
+            GLES20.GlBlendFunc(GLES20.GlOne, GLES20.GlOneMinusSrcAlpha);
+            GLES20.GlDrawElements(
+                GLES20.GlTriangles, triangleIndices.Limit(), GLES20.GlUnsignedShort, triangleIndices);
+
+            GLES20.GlUseProgram(0);
+            GLES20.GlDepthMask(true);
+        }
+
+
+        public void SetMaterialProperties(float ambient, float diffuse, float specular, float specularPower)
+        {
+            this.ambient = ambient;
+            this.diffuse = diffuse;
+            this.specular = specular;
+            this.specularPower = specularPower;
+        }
+
+        private static void NormalizeVec3(float[] v)
+        {
+            float reciprocalLength = 1.0f / (float)Math.Sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+            v[0] *= reciprocalLength;
+            v[1] *= reciprocalLength;
+            v[2] *= reciprocalLength;
+        }
+    }
+}
